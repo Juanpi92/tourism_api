3000b3023ac9675f24c80ae4336fcafd3e94450a
Francisco <113522254+WillianXG@users.noreply.github.com>
2023-03-16 11:47:51 -0300

    table history / Token JWT

--- src/documentation/user.js
+++ src/documentation/user.js
@@ -10,6 +10,9 @@ const btn3 = document.getElementById('btn3')
 const purchase = document.querySelector('.examplesOff4')
 const btn4 = document.getElementById('btn4')
 
+const historico = document.querySelector('.examplesOff5')
+const btn5 = document.getElementById('btn5')
+
 btn1.addEventListener('click', ()=>{
     register.classList.toggle('examplesOn')
 })
@@ -24,4 +27,8 @@ btn3.addEventListener('click', ()=>{
 
 btn4.addEventListener('click', ()=>{
     purchase.classList.toggle("examplesOn")
+})
+
+btn5.addEventListener('click', ()=>{
+    historico.classList.toggle("examplesOn")
 })
